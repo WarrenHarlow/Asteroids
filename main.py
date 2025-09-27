43417f22d6c3e6e1ae05e10cc9d9fa41e9ce2143
WarrenHarlow <Warren.Harlow@gmail.com>
2025-09-26 22:23:41 -0400

    got triangle on screen

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 
+# python
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    #game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill((0, 0, 0))  # Fill the screen with black
-        pygame.display.flip()  # Update the full display Surface to the screen
-        
-        
 
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
